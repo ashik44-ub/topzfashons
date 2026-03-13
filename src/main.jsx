17f9fe8ac605988d51aa1fc1d0253c85e85ee162
Ashikur Rahman <shopnilashik4@gmail.com>
2026-03-13 23:24:44 +0600

    Fix: 401 Unauthorized error by adding withCredentials and SameSite cookie settings

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,23 @@ import { persistor, store } from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 import { Toaster } from 'react-hot-toast';
 
+// --- AXIOS CONFIGURATION START ---
+import axios from 'axios';
+
+// Backend-e cookie pathanor jonno eta MUST
+axios.defaults.withCredentials = true; 
+
+// Base URL set kore nite paren (Optional, kintu bhalo practice)
+axios.defaults.baseURL = 'https://topzfashions-backend.vercel.app'; 
+// --- AXIOS CONFIGURATION END ---
+
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Toaster position="top-center" reverseOrder={false} />
-    <RouterProvider router={router}/>
-    </PersistGate>
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster position="top-center" reverseOrder={false} />
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 )
